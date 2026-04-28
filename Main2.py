64a2760465eaf8eb7746ad176b38053a324d19ea
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-04-28 19:18:28 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main2.py
@@ -0,0 +1,7 @@
+# 1.Python Program to Find the Union of Two Lists.
+list1 = [1, 2, 3, 4]
+list2 = [3, 4, 5, 6]
+union = list(set(list1 + list2))
+print("List 1 :", list1)
+print("List 2 :", list2)
+print("Union of Two Lists :", union)
