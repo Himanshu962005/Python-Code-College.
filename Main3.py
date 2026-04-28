a48d5970747bae217bccf013efb5280f73035340
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-04-28 19:27:58 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main3.py
@@ -0,0 +1,7 @@
+# Q2.Python Program to Find the Intersection of Two Lists.
+list1 = [1, 2, 3, 4]
+list2 = [3, 4, 5, 6]
+intersection = list(set(list1) & set(list2))
+print("List 1 :", list1)
+print("List 2 :", list2)
+print("Intersection of Two Lists :", intersection)
