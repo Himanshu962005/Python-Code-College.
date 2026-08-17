82f3bb0657f4307ad3c213a1b6f6e45862e0bfa3
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-08-17 17:30:47 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main16.py
@@ -0,0 +1,5 @@
+# Q15.Write a Method to Calculate GCD of Two Numbers.
+import math;
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+print("GCD =", math.gcd(a, b))
