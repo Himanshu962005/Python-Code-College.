da9c804f981da24b270cd76c71abb8f5aeffa102
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-08-17 17:46:24 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main18.py
@@ -0,0 +1,8 @@
+# Q17.Write a Program to Create Queue Class and Implement All its Methods (Use Lists).
+queue = []
+queue.append(10)
+queue.append(20)
+queue.append(30)
+print("Queue :", queue)
+print("Deleted :", queue.pop(0))
+print("After Delete :", queue)
