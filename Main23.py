65777d2be01206c7e0f3e1f41652957f4fa430d3
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-08-17 20:23:48 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main23.py
@@ -0,0 +1,7 @@
+# Q22.Python Program to Check if a Substring is Present in a Given String.
+string = input("Enter Main String : ")
+sub = input("Enter Substring : ")
+if sub in string:
+    print("Substring Present")
+else:
+    print("Substring Not Present")
