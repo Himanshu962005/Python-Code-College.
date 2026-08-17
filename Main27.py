bdaf829f10ff27dae404211fb5244d7488baaddd
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-08-17 23:04:14 +0530

    Delete Main27.py
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

--- Main27.py
+++
@@ -1,7 +0,0 @@
-# Q26.Python Program to Find the Length of a List Using Recursion.
-def list_length(lst):
-    if not lst:
-        return 0
-    return 1 + list_length(lst[1:])
-lst = [1, 2, 3, 4, 5]
-print("Length =", list_length(lst))
