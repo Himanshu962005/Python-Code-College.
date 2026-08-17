8a7538d2ea2964a14f34a87ac4b4c7b2556d0ece
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-08-17 17:38:37 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main17.py
@@ -0,0 +1,8 @@
+# Q16.Write a Program to Create Stack Class and Implement All its Methods (Use Lists).
+stack = []
+stack.append(10)
+stack.append(20)
+stack.append(30)
+print("Stack :", stack)
+print("Popped :", stack.pop())
+print("After Pop :", stack)
