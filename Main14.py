02c7cca9b2645c3b36c0c64d96070c34fe4d0cf4
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-08-17 17:13:48 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main14.py
@@ -0,0 +1,6 @@
+# Q13.Write a Program to Compare Three Numbers and Print the Largest One.
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+c = int(input("Enter Third Number : "))
+largest = max(a, b, c)
+print("Largest Number = ", largest)
