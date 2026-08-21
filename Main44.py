3a4ed4ba1423f5970dffbc3619c1d43d9785307c
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-08-21 22:51:39 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main44.py
@@ -0,0 +1,7 @@
+# E1.Create a Calculator Capable of Performing Addition, Subtraction, Multiplication and Division Operations on Two Numbers. Your Program should Format the Output in a Readable Manner!.
+A = 9
+B = 6
+print("The Value of", A, "+", B, "is :", A + B)
+print("The Value of", A, "-", B, "is :", A - B)
+print("The Value of", A, "*", B, "is :", A * B)
+print("The Value of", A, "/", B, "is :", A / B)
