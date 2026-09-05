c1d9752845acd043475d0cd99dc43586134e661d
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-09-05 14:14:54 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main62.py
@@ -0,0 +1,6 @@
+# E19.'is' VS '==' in Python.
+a = None
+b = None
+print(a is b)  # Exact Location of Object in Memory.
+print(a is None)  # Exact Location of Object in Memory.
+print(a == b)  # Value.
