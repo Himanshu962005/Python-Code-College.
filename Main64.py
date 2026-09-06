9b46fe28488e23547c5058af5205f8b89aabb526
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-09-06 21:43:18 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main64.py
@@ -0,0 +1,23 @@
+# E21.Introduction to OOPs in Python.
+def hello():
+    print("Hello")
+hello()
+sales1 = 6000
+profit1 = 2000
+ad1 = 1000
+
+# Rajeev-Sales.
+sales2 = 6000
+profit2 = 2000
+ad2 = 1000
+
+# Vikrant-Sales.
+sales3 = 6000
+profit3 = 2000
+ad3 = 1000
+
+# Railway Form ---> Class [Blueprint].
+# Himanshu ---> Himanshu Information Form ---> Object [Entity].
+# Tom ---> Tom Information Form ---> Object [Entity].
+# Shubham ---> Shubham Information Form ---> Object [Entity].
+# shubham.changeName("Shubhi").
