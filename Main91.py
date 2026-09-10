d7bd6757e1190e98816559c2b57ca433abcafe99
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-09-10 17:22:33 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main91.py
@@ -0,0 +1,8 @@
+# E48.Shutil Module in Python.
+import shutil;
+import os;
+# shutil.copy("Main91.py", "Main91_Copy.py").
+# shutil.copytree(".tutorial", "MyTutorial").
+# shutil.move(".tutorial/file.file", "File.file").
+# shutil.rmtree("MyTutorial").
+os.remove("Main91_Copy.py")
