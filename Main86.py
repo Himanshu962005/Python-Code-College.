1ab0d415174182b735a56ebbc6b9d9b296692b3d
HIMANSHU KARWA <valthimanshu@gmail.com>
2026-09-10 14:27:20 +0530

    Add files via upload
    Signed-off-by: HIMANSHU KARWA <valthimanshu@gmail.com>

---
+++ Main86.py
@@ -0,0 +1,19 @@
+# E43.Hybrid and Hierarchical Inheritance in Python.
+# Example of Hybrid Inheritance.
+class BaseClass:
+    pass
+class Derived1(BaseClass):
+    pass
+class Derived2(BaseClass):
+    pass
+class Derived3(Derived1, Derived2):
+    pass
+# Example of Hierarchical Inheritance.
+class BaseClass:
+    pass
+class D1(BaseClass):
+    pass
+class D2(BaseClass):
+    pass
+class D3(D1):
+    pass
